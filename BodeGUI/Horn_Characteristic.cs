73867b463656d9ae1d28b0b17c474f66ecec850e
grapp2 <grapp@pneumarespiratory.com>
2023-01-20 14:41:43 -0500

    capacitance fixed

--- BodeGUI/Horn_Characteristic.cs
+++ BodeGUI/Horn_Characteristic.cs
@@ -84,19 +84,17 @@ namespace BodeGUI
             horn_data.Res_impedance = Math.Round(horn_data.Res_impedance, 3, MidpointRounding.AwayFromZero);
             horn_data.Anti_impedance = Math.Round(horn_data.Anti_impedance / 1000.0, 3, MidpointRounding.AwayFromZero);
 
-            measurement.ConfigureSweep(1000, 1201, 51, SweepMode.Logarithmic);
+            measurement.ConfigureSinglePoint(1000);
+            state = measurement.ExecuteMeasurement();
+
             if (state != ExecutionState.Ok)
             {
                 bode.ShutDown();
                 return;
             }
             freq = measurement.Results.MeasurementFrequencies;
-            List<double> cap = new List<double>();
-            foreach(var a in measurement.Results.Cs())
-            {
-                cap.Add(a);
-            }
-            horn_data.Capacitance = cap[0];
+            double cap = measurement.Results.CsAt(0);
+            horn_data.Capacitance = cap;
 
         }
 
